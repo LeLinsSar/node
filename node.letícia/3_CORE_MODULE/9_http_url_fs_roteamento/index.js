56d3dd5a91cb3fa58702fba0aa7f1e92f615e94b
Letícia <110118449+LeLinsSar@users.noreply.github.com>
2023-07-05 16:04:19 -0300

    Add files via upload

---
+++ node.letícia/3_CORE_MODULE/9_http_url_fs_roteamento/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const PORT = 3000
+
+const server = http.createServer((req, res)=>{
+    const q = url.perse(req.url, true);
+    const fileName = q.pathname.substring(1);
+    //localhost:3000/index.html
+    //                0   .  1
+        if(fileName.includes('html')){
+            if(fs.existsSync(fileName)){
+                fs.readFile(fileName, function (err, data){
+                    res.writeHead(200, {"Content-Type":"text/html"});
+                    res.write(data);
+                    return res.end();
+                });
+                }else{
+                    
+                }
+        }
+
+});
+
+server.listen(PORT, ()=>{
+    console.log(`Servidor on na porta: ${PORT}😜`);
+});
